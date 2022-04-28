57626f6d665fb32443592003f99b91413e597ad9
Elad Inbar <eladin@post.bgu.ac.il>
2022-04-28 14:49:31 +0300

    Rename DataObjs.ts to DataObjects.ts

--- dev/src/domain/controller/SystemController.ts
+++ dev/src/domain/controller/SystemController.ts
@@ -1,6 +1,6 @@
 import {Guest} from "../user/Guest";
 import {SecurityController} from "./SecurityController";
-import {loginData, newRoleData, newShopData, registerMemberData} from "../../utilities/DataObjs";
+import {loginData, newRoleData, newShopData, registerMemberData} from "../../utilities/DataObjects";
 import {Member} from "../user/Member";
 import {Id} from "../../utilities/Utils";
 import {Role} from "../user/Role";
