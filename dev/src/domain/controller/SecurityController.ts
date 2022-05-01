4f84bf04d408c3a010a0ffd6f7479406e0489512
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-01 14:35:55 +0300

    Implemented 'accessMarketplace' 'exitMarketplace' and 'isLoggedIn' in 'SecurityController.ts'

--- dev/src/domain/controller/SecurityController.ts
+++ dev/src/domain/controller/SecurityController.ts
@@ -12,12 +12,21 @@ export class SecurityController {
         this.loggedInMembers = new Array<string>();
     }
 
-    accessMarketplace(guestID: string) {
+    accessMarketplace(guestID: string): void {
+        if(this.activeGuests.includes(guestID))
+            throw new Error(`There already exists a guest with ${guestID} in the marketplace`);
 
+        this.activeGuests.push(guestID);
     }
 
-    exitMarketplace(guestID: string) {
+    exitMarketplace(guestID: string): void {
+        if(!this.activeGuests.includes(guestID))
+            throw new Error(`There is no guest with ${guestID} currently in the marketplace`);
 
+        const index = this.activeGuests.indexOf(guestID);
+        if (index > -1) {
+            this.activeGuests.splice(index, 1);
+        }
     }
 
     register(username: string, password: string): void {
@@ -29,16 +38,18 @@ export class SecurityController {
         this.users.set(username, password);
     }
 
-    login(username: string, password: string) {
+    login(username: string, password: string): void {
         if(!this.users.has(username))
             throw new Error(`A member with the username '${username}' does not exist`);
+        if(this.loggedInMembers.includes(username))
+            throw new Error(`The member ${username} is already logged into the system`)
         if(this.users.get(username) != password)
             throw new Error(`The password is invalid, please try again`);
 
         this.loggedInMembers.push(username);
     }
 
-    logout(username: string) {
+    logout(username: string): void {
         if(!this.users.has(username))
             throw new Error(`A member with the username '${username}' does not exist`);
         if(!this.loggedInMembers.includes(username))
@@ -50,7 +61,7 @@ export class SecurityController {
         }
     }
 
-    isLoggedIn(userID: string) {
-
+    isLoggedIn(userID: string): boolean {
+        return !(!this.loggedInMembers.includes(userID) || !this.activeGuests.includes(userID));
     }
 }
