74cc643795354498993ce345a27aad9f32948c12
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-01 14:08:18 +0300

    Added implementation for 'login' and 'logout'. changed 'activeGuests' and 'loggedInMembers' types to string[] to decouple and simplify usage.

--- dev/src/domain/controller/SecurityController.ts
+++ dev/src/domain/controller/SecurityController.ts
@@ -1,17 +1,15 @@
-import {User} from "../user/User";
-import {Member} from "../user/Member";
 
 export class SecurityController {
     private readonly MINIMUM_PASSWORD_LENGTH = 8;
 
     private users: Map<string, string>;
-    private activeGuests: User[];
-    private loggedInMembers: Member[];
+    private activeGuests: string[];
+    private loggedInMembers: string[];
 
     constructor() {
         this.users = new Map<string, string>();
-        this.activeGuests = new Array<User>();
-        this.loggedInMembers = new Array<Member>();
+        this.activeGuests = new Array<string>();
+        this.loggedInMembers = new Array<string>();
     }
 
     accessMarketplace(guestID: string) {
@@ -32,11 +30,24 @@ export class SecurityController {
     }
 
     login(username: string, password: string) {
+        if(!this.users.has(username))
+            throw new Error(`A member with the username '${username}' does not exist`);
+        if(this.users.get(username) != password)
+            throw new Error(`The password is invalid, please try again`);
 
+        this.loggedInMembers.push(username);
     }
 
     logout(username: string) {
+        if(!this.users.has(username))
+            throw new Error(`A member with the username '${username}' does not exist`);
+        if(!this.loggedInMembers.includes(username))
+            throw new Error(`The member ${username} is not currently logged in`);
 
+        const index = this.loggedInMembers.indexOf(username);
+        if (index > -1) {
+            this.loggedInMembers.splice(index, 1);
+        }
     }
 
     isLoggedIn(userID: string) {
