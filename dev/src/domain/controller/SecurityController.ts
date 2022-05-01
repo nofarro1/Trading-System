81fbbf23a6ac903e0bb4fbb39304dd2cfb1fdb0c
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-01 13:21:49 +0300

    Initialized the 'SecurityController.ts' class. Added field access modifiers, function signatures and constructor.

--- dev/src/domain/controller/SecurityController.ts
+++ dev/src/domain/controller/SecurityController.ts
@@ -3,13 +3,35 @@ import {Member} from "../user/Member";
 
 
 export class SecurityController {
+    private activeGuests: User[];
+    private loggedInMembers: Member[];
 
+    constructor() {
+        this.activeGuests = new Array<User>();
+        this.loggedInMembers = new Array<Member>();
+    }
 
-    activeGuests: User[] = [];
-    loggedMembers: Member[] = [];
+    accessMarketplace(guestID: string) {
 
+    }
 
-    addActiveGuest(newGuest: User) {
+    exitMarketplace(guestID: string) {
+
+    }
+
+    register(username: string, password: string) {
+
+    }
+
+    login(username: string, password: string) {
+
+    }
+
+    logout(username: string) {
+
+    }
+
+    isLoggedIn(userID: string) {
 
     }
 }
