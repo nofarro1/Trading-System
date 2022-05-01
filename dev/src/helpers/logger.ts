2e70428cb32d896dda3bc948e8d74dd2e408d17e
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-01 20:34:00 +0300

    Implemented winston logger.

--- dev/src/helpers/logger.ts
+++ dev/src/helpers/logger.ts
@@ -1,8 +1,17 @@
- 
-import { createLogger, format, transports, config } from 'winston';
- 
-export const logger = createLogger({
-   transports: [
-       new transports.Console()
-     ]
- });
+import winston from "winston";
+
+const logFormat = winston.format.printf(({ level, message, timestamp, stack }) => {
+    return `${timestamp} ${level}: ${stack || message}`;
+});
+
+const format = winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    winston.format.errors({ stack: true }),
+    logFormat);
+
+const transports = [new winston.transports.Console({format: winston.format.colorize({ all: true })}),
+    new winston.transports.File({ filename: 'error.log', level: 'error'}),
+    new winston.transports.File({ filename: 'event.log', level: 'info'}),
+];
+
+export const logger = winston.createLogger({ format, transports });
