d1d2509ad67de5a8c15405464846bf8ba9124ca6
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-01 13:44:03 +0300

    Implemented 'register()' in 'SecurityController.ts'

--- dev/src/domain/controller/SecurityController.ts
+++ dev/src/domain/controller/SecurityController.ts
@@ -1,12 +1,15 @@
 import {User} from "../user/User";
 import {Member} from "../user/Member";
 
-
 export class SecurityController {
+    private readonly MINIMUM_PASSWORD_LENGTH = 8;
+
+    private users: Map<string, string>;
     private activeGuests: User[];
     private loggedInMembers: Member[];
 
     constructor() {
+        this.users = new Map<string, string>();
         this.activeGuests = new Array<User>();
         this.loggedInMembers = new Array<Member>();
     }
@@ -19,8 +22,13 @@ export class SecurityController {
 
     }
 
-    register(username: string, password: string) {
+    register(username: string, password: string): void {
+        if(this.users.has(username))
+            throw new Error(`A member with the username ${username} already exists`);
+        if(password.length < this.MINIMUM_PASSWORD_LENGTH)
+            throw new RangeError(`Password is too short and must contain at least ${this.MINIMUM_PASSWORD_LENGTH} characters`);
 
+        this.users.set(username, password);
     }
 
     login(username: string, password: string) {
