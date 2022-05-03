1c4339918571fab88b356948c290822016dde1e6
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-03 09:15:07 +0300

    Basic implementation of the external services.

--- dev/src/domain/external_services/DeliveryServiceAdaptor.ts
+++ dev/src/domain/external_services/DeliveryServiceAdaptor.ts
@@ -1,5 +1,11 @@
+import {Result} from "../../utilities/Result";
+import {logger} from "../../helpers/logger";
 
 
 export class DeliveryServiceAdaptor {
 
+    makeDelivery(deliveryDetails: any): Result<void> {
+        logger.info("Delivery has been made successfully");
+        return new Result<void>(true, undefined, "Delivery was successful");
+    }
 }
--- dev/src/domain/external_services/PaymentServiceAdaptor.ts
+++ dev/src/domain/external_services/PaymentServiceAdaptor.ts
@@ -1,5 +1,11 @@
+import {logger} from "../../helpers/logger";
+import {Result} from "../../utilities/Result";
 
 
 export class PaymentServiceAdaptor {
 
+    makePayment(paymentDetails: any): Result<void> {
+        logger.info("Payment has been made successfully");
+        return new Result<void>(true, undefined, "Payment was successful");
+    }
 }
