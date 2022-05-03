5e08d699edc33ea5695930c9aab10314be516731
Elad Inbar <eladin@post.bgu.ac.il>
2022-05-03 13:38:54 +0300

    Simplified external service requests to only allow a single service of some type to have a concurrent connection.

--- dev/src/service/OrderService.ts
+++ dev/src/service/OrderService.ts
@@ -10,24 +10,13 @@ export class OrderService {
         this.systemController = systemController;
     }
 
-    //System - Use-Case 2
+    //System - Use-Case 2/2.2
     addConnectionWithExternalService(type: ExternalServiceType, serviceName: string): Result<void> {
         return this.systemController.addConnectionWithExternalService(type, serviceName);
     }
 
     //System - Use-Case 2.1
-    editConnectionWithExternalService(type: ExternalServiceType, serviceName: string, settings: any): Result<void> {
-        return this.systemController.editConnectionWithExternalService(type, serviceName, settings);
-    }
-
-    //System - Use-Case 2.2
-    swapConnectionWithExternalService(type: ExternalServiceType, oldServiceName: string, newServiceName: string): Result<void> {
-        return this.systemController.swapConnectionWithExternalService(type, newServiceName);
-    }
-
-    //System - Use-Case 3
-    //System - Use-Case 4
-    callService(type: ExternalServiceType, serviceDetails?: any): Result<void> {
-        return this.systemController.callService(type, serviceDetails);
+    editConnectionWithExternalService(type: ExternalServiceType, settings: any): Result<void> {
+        return this.systemController.editConnectionWithExternalService(type, settings);
     }
 }
