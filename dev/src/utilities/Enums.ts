b4bffadf01ba68ab3d91cace819980de21b49b4f
ofirpovi <ofirpovi@bgu.ac.il>
2022-05-03 10:45:18 +0300

    Changed the enums names.

--- dev/src/utilities/Enums.ts
+++ dev/src/utilities/Enums.ts
@@ -1,9 +1,9 @@
-export enum searchType{productName, category, keyword}
-export enum sortType{price, productRate, category, shopRate}
-export enum shopRate {NotRated};
-export enum productCategory{};
-export enum productRate{NotRated};
-export enum shopStatus{open, close};
+export enum SearchType{productName, category, keyword}
+export enum SortType{price, productRate, category, shopRate}
+export enum ShopRate {NotRated};
+export enum ProductCategory{};
+export enum ProductRate{NotRated};
+export enum ShopStatus{open, close};
 
 export enum JobType {
     Founder,
