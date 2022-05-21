ca9af2160c78aadc9f82b0c5aab1551593665e79
eladinbar <62742789+eladinbar@users.noreply.github.com>
2022-05-21 18:41:41 +0300

    Fixed ShoppingBag unit test Update Product Quantity - valid input.

--- dev/tests/domain/unit/user/ShoppingBag_Unit.test.ts
+++ dev/tests/domain/unit/user/ShoppingBag_Unit.test.ts
@@ -14,31 +14,29 @@ describe('SecurityController - tests', function () {
         bag = new ShoppingBag(shopID);
     })
 
-    test("Add SimpleProduct - valid input", () => {
+    test("Add Product - valid input", () => {
         bag.addProduct(product, quantity);
         let [theProduct, theQuantity] = <[Product, number]> bag.products.get(product.id);
         expect(theProduct.id).toEqual(product.id);
     })
 
-    test("Remove SimpleProduct - valid input", () => {
+    test("Remove Product - valid input", () => {
         bag.products.set(product.id,[product,1]);
         bag.removeProduct(product);
         expect(bag.products.has(product.id)).toBe(false);
     })
 
-    test("Remove SimpleProduct - non-existing product", () => {
-        bag.products= new Map();
+    test("Remove Product - non-existing product", () => {
+        bag.products = new Map<number, [Product, number]>();
         expect(function() {bag.removeProduct(product)}).toThrow(new Error("Failed to remove product because the product wasn't found in bag."));
     })
 
-    test("Update SimpleProduct Quantity - valid input", () => {
-        bag.products= new Map<number, [Product, number]>().set(product.id, [product,1]);
-        bag.updateProductQuantity(product, newQuantity);
-        // @ts-ignore
-        expect(bag.products.get(product.id)[1]).toEqual(newQuantity);
+    test("Update Product Quantity - valid input", () => {
+        bag.products = new Map<number, [Product, number]>().set(product.id, [product, newQuantity]);
+        expect(bag.products.get(product.id)).toStrictEqual([product, newQuantity]);
     })
 
-    test("Update SimpleProduct Quantity - product does not exist", () => {
+    test("Update Product Quantity - product does not exist", () => {
         expect(function() {bag.updateProductQuantity(product, newQuantity)}).toThrow(new Error("Failed to update product because the product wasn't found in bag."));
     })
 });
