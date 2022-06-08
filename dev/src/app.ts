c25d811fdb8246462e2adadfce61d814bb16db5f
eladinbar <62742789+eladinbar@users.noreply.github.com>
2022-06-08 16:57:53 +0300

    initial commit

--- dev/src/app.ts
+++ dev/src/app.ts
@@ -2,6 +2,7 @@ import {app} from "./Server/expressApp";
 import {systemContainer} from "./helpers/inversify.config";
 import {TYPES} from "./helpers/types";
 import {Server} from "./Server/Server";
+import 'reflect-metadata';
 
 const theServer = new Server(app,systemContainer.get(TYPES.Service));
 theServer.start()
