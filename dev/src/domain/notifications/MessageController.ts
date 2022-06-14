fdc85018342481f931fca8baec7ccad224fb3bb1
ofirpovi <ofirpovi@bgu.ac.il>
2022-06-14 14:42:06 +0300

    Added offers functionality to the service and the system controller.

--- dev/src/domain/notifications/MessageController.ts
+++ dev/src/domain/notifications/MessageController.ts
@@ -1,5 +1,5 @@
 import {MessageBox, ILLiveNotificationSubscriber} from "./MessageBox";
-import {Message, ShopPurchaseMessage, ShopStatusChangedMessage} from "./Message";
+import {AddedNewOffer2ShopMessage, Message, ShopPurchaseMessage, ShopStatusChangedMessage} from "./Message";
 import {IMessageListener} from "./IEventPublishers";
 import {Result} from "../../utilities/Result";
 import {injectable} from "inversify";
@@ -87,6 +87,13 @@ export class MessageController implements IMessageListener<Message> {
     }
 
     visitShopStatusChangedEvent(msg: ShopStatusChangedMessage): void {
+        let recipients = msg.recipients;
+        for (let key of recipients) {
+            this.messageBoxes.get(key)?.addMessage(msg)
+        }
+    }
+
+    visitNewShopOffer(msg: AddedNewOffer2ShopMessage): void {
         let recipients = msg.recipients;
         for (let key of recipients) {
             this.messageBoxes.get(key)?.addMessage(msg)
