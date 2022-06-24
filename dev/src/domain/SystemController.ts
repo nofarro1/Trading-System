cf828f3f280601de2072b5b28c4201ecc2b4568d
Edan Levy <edan.levy@gmail.com>
2022-06-24 12:58:56 +0300

    fixed mage with dev3

--- dev/src/domain/SystemController.ts
+++ dev/src/domain/SystemController.ts
@@ -744,12 +744,49 @@ export class SystemController {
         })
     }
 
-    addOffer2Shop(sessionId, userName: string, shopId: number, pId: number, price: number): Result<void> {
+    /*-----------------------------------Offer (bid on product)----------------------------------------------*/
+    addOffer2Shop(sessionId, shopId: number, pId: number, price: number) : Result<void>{
         return this.authenticateMarketVisitor(sessionId, (username) => {
-            let offer: Result<void | Offer> = this.mpController.addOffer2Product(shopId, userName, pId, price);
-            if (checkRes(offer)) {
+            let offer: Result<void | Offer> = this.mpController.addOffer2Product(shopId, username, pId, price);
+            if(checkRes(offer)){
                 return this.scController.addOffer2cart(username, offer.data);
             }
         })
     }
+
+    approveOffer(sessionId: string, shopId: number, offerId: number, answer: boolean) :Result<void>{
+        return this.authenticateMarketVisitor(sessionId, (username)=>{
+           return this.mpController.approveOffer(shopId, offerId, username, answer);
+        });
+    }
+
+    filingCounterOffer(sessionId: string, shopId: number, offerId: number, counterPrice: number): Result<void>{
+        return this.authenticateMarketVisitor(sessionId, (username)=>{
+            let result: Result<void | Offer> =  this.mpController.filingCounterOffer(shopId, offerId, username, counterPrice);
+            if (checkRes(result)) {
+                this.scController.updateOfferFromCart(result.data);
+                return Result.Ok(result.data);
+            }
+            return Result.Fail(result.message);
+        })
+    }
+
+    denyCounterOffer(sessionId: string, username: string, shopId: number, offerId: number): Result<void>{
+        return this.authenticateMarketVisitor(sessionId, ()=>{
+            this.scController.removeOffer(username, offerId);
+            return this.mpController.denyCounterOffer(shopId, offerId);
+        })
+    }
+
+    acceptCounterOffer(sessionId: string, shopId: number, offerId: number): Result<void>{
+        return this.authenticateMarketVisitor(sessionId, (username)=>{
+            let result: Result<void | Offer> = this.mpController.acceptCounterOffer(shopId, offerId);
+            if (checkRes(result)) {
+                this.scController.updateOfferFromCart(result.data);
+                return Result.Ok(result.data);
+            }
+            return Result.Fail(result.message);
+        })
+    }
+
 }
